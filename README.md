8d7ac1c1c938d3fc14d964d9bb00c13b037e79ad
Calvin Furano <cfurano@solarcity.com>
2016-11-15 17:12:11 -0700

    Round 1

---
+++ README.md
@@ -0,0 +1,47 @@
+Code Golf Round 1
+===========================
+
+Getting Started
+---------------
+
+### Introduction
+
+Code Golf is a competition where given a programming prompt, the shortest source code (measured in bytes) to fully implement the prompt wins the challenge.
+
+### Challenge
+
+Write a program that adds together several small positive integers. **Your program may not use the `+` character.**
+
+The integers are provided through `stdin`. They are seperated by a single space
+
+```bash
+$ echo 42 13 | node my-solution.js
+55
+$ echo 128 256 512 | python3 my-solution.py
+896
+$ echo 9 8 7 6 5 4 3 2 1 | ruby my-solution.rb
+45
+``` 
+
+### Rules
+
+The languages are limited to JavaScript (Node.js), Python3, and Ruby. You may not use libraries. Your submission is limited to a single file. That file cannot contain the `+` character.
+
+### Scoring
+
+Please submit a single file (.js, .py, or .rb). The included node script `code-golf.sh` will be used to score your file. To run this script, you must have Node.js installed.
+
+macOS
+```bash
+$ ./code-golf.sh my-solution.js
+Your score is 456
+```
+
+Windows
+```
+C:\> node code-golf.sh my-solution.js
+Your score is 456
+```
+
+
+![golf](https://media.giphy.com/media/Moo8SpSUk6R4A/giphy.gif)
---
+++ code-golf.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+"use strict"
+const fs = require("fs")
+const spawn = require('child_process').spawn
+const path = require("path")
+const assert = require("assert")
+
+let args = process.argv
+let file = args[2]
+if (!file) {
+    console.log("Must provide file as first argument")
+    process.exit(1)
+}
+
+let stat = fs.statSync(file)
+
+let fileContents = fs.readFileSync(file, "utf-8")
+checkFileContents(fileContents)
+
+let extension = path.extname(file)
+
+let runtime
+switch (extension) {
+    case ".js":
+        runtime = "node"
+        break
+    case ".py":
+        runtime = "python3"
+        break
+    case ".rb":
+        runtime = "ruby"
+        break
+    default:
+        console.log("Unrecognized file type")
+        process.exit(1)
+}
+
+test(runtime, file).then(() => { 
+    console.log(`Your score is ${stat.size}`)
+    process.exit(0)
+}).catch((e) => { 
+    console.log("Failed tests")
+    process.exit(1)
+})
+
+function test(runtime, file) {
+    return Promise.all([
+        runTest(runtime, file, "8 10", "18"),
+        runTest(runtime, file, "200 42 1024", "1266"),
+        runTest(runtime, file, "9 8 7 6 5 4 3 2 1", "45"),
+        runTest(runtime, file, "128 256 512", "896")
+    ])
+}
+
+function runTest(runtime, file, input, expectedOutput) {
+    return new Promise((resolve, reject) => {
+        let timeout = setTimeout(function () {
+            reject("file failed tests")
+        }, 1000)
+        let node = spawn(runtime, [file])
+        node.stdin.write(input)
+        node.stdout.setEncoding('utf8')
+        node.stdout.on("data", (data) => {
+            clearTimeout(timeout)
+            assert.equal(expectedOutput, data.trim())
+            resolve("file passed tests")
+        })
+    })
+}
+
+function checkFileContents(fileContents) {
+    assert(!fileContents.includes("+"), "file contains a + character")
+}
